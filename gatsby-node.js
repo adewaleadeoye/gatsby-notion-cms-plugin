aa5cd00039b479bd473386b0ca0f0f4890e9bc44
Adewale Adeoye <whaale_ade@yahoo.com>
2022-05-27 05:42:32 +0100

    setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,50 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-// You can delete this file if you're not using it
+const axios = require('axios').default;
+const { URL } = require('url');
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+const getNotionArticles = async (baseUrl, notion_secret, notion_database_id) => {
+  try {
+    const resp = await axios({
+      url: `${baseUrl}/databases/${notion_database_id}/query`,
+      method: 'POST',
+      headers: {
+        authorization: `Bearer ${notion_secret}`,
+        'Notion-Version': '2022-02-22',
+      },
+    });
+
+    return resp.data?.results.map((result) => {
+      return {
+        id: result.id,
+        title: result.properties.Name.title[0].text.content,
+        excerpt: result.properties.Description.rich_text[0].text.content,
+        date: result.properties.Date.rich_text[0].text.content,
+        image: result.properties.Image.files[0].file.url,
+        url: new URL(result.url).pathname,
+      };
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+exports.sourceNodes = async (
+  { actions, createContentDigest, createNodeId },
+  { baseUrl, notion_secret, notion_database_id }
+) => {
+  const ARTICLE_TYPE = 'EukaPayNews';
+  const articles = await getNotionArticles(baseUrl, notion_secret, notion_database_id);
+  for (const article of articles) {
+    actions.createNode({
+      ...article,
+      id: createNodeId(`${ARTICLE_TYPE}-${article.id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: ARTICLE_TYPE,
+        contentDigest: createContentDigest(article),
+      },
+    });
+  }
+};
+
+exports.onPreInit = () => console.log('Loaded gatsby-starter-plugin');
